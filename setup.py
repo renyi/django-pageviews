597c56462140edec5842a85d07c92d093023a3bf
Renyi Khor <renyi.ace@gmail.com>
2014-01-14 07:09:34 +0800

    Added setup.py. Closes #1.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-pageviews',
+    version='0.1',
+    description='Django Pageviews',
+    author='Renyi Khor',
+    author_email='renyi.ace@gmail.com',
+    packages=find_packages(),
+    include_package_data = True,
+)
